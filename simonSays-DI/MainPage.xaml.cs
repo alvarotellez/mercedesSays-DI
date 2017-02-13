9420943bb9d9f70b470a761b1346af8a3cc54ff5
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2017-02-13 12:25:32 +0100

    jkafjkfasdjkfas

--- simonSays-DI/MainPage.xaml.cs
+++ simonSays-DI/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +27,81 @@ namespace simonSays_DI
         {
             this.InitializeComponent();
         }
+        private void comboNivel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (op1.IsSelected)
+            {
+                rec36.Fill = new SolidColorBrush(Colors.LightGray);
+                rec35.Fill = new SolidColorBrush(Colors.LightGray);
+                rec34.Fill = new SolidColorBrush(Colors.LightGray);
+                rec33.Fill = new SolidColorBrush(Colors.LightGray);
+                rec32.Fill = new SolidColorBrush(Colors.LightGray);
+                rec31.Fill = new SolidColorBrush(Colors.LightGray);
+                rec17.Fill = new SolidColorBrush(Colors.LightGray);
+                rec30.Fill = new SolidColorBrush(Colors.LightGray);
+                rec18.Fill = new SolidColorBrush(Colors.LightGray);
+                rec29.Fill = new SolidColorBrush(Colors.LightGray);
+                rec19.Fill = new SolidColorBrush(Colors.LightGray);
+                rec28.Fill = new SolidColorBrush(Colors.LightGray);
+                rec20.Fill = new SolidColorBrush(Colors.LightGray);
+                rec27.Fill = new SolidColorBrush(Colors.LightGray);
+                rec21.Fill = new SolidColorBrush(Colors.LightGray);
+                rec22.Fill = new SolidColorBrush(Colors.LightGray);
+                rec23.Fill = new SolidColorBrush(Colors.LightGray);
+                rec24.Fill = new SolidColorBrush(Colors.LightGray);
+                rec25.Fill = new SolidColorBrush(Colors.LightGray);
+                rec26.Fill = new SolidColorBrush(Colors.LightGray);
+
+                rec5.Fill = new SolidColorBrush(Colors.LightGray);
+                rec16.Fill = new SolidColorBrush(Colors.LightGray);
+                rec15.Fill = new SolidColorBrush(Colors.LightGray);
+                rec14.Fill = new SolidColorBrush(Colors.LightGray);
+                rec6.Fill = new SolidColorBrush(Colors.LightGray);
+                rec13.Fill = new SolidColorBrush(Colors.LightGray);
+                rec7.Fill = new SolidColorBrush(Colors.LightGray);
+                rec12.Fill = new SolidColorBrush(Colors.LightGray);
+                rec8.Fill = new SolidColorBrush(Colors.LightGray);
+                rec9.Fill = new SolidColorBrush(Colors.LightGray);
+                rec10.Fill = new SolidColorBrush(Colors.LightGray);
+                rec11.Fill = new SolidColorBrush(Colors.LightGray);
+
+            }
+
+
+            if (op2.IsSelected)
+            {
+                rec36.Fill = new SolidColorBrush(Colors.LightGray);
+                rec35.Fill = new SolidColorBrush(Colors.LightGray);
+                rec34.Fill = new SolidColorBrush(Colors.LightGray);
+                rec33.Fill = new SolidColorBrush(Colors.LightGray);
+                rec32.Fill = new SolidColorBrush(Colors.LightGray);
+                rec31.Fill = new SolidColorBrush(Colors.LightGray);
+                rec17.Fill = new SolidColorBrush(Colors.LightGray);
+                rec30.Fill = new SolidColorBrush(Colors.LightGray);
+                rec18.Fill = new SolidColorBrush(Colors.LightGray);
+                rec29.Fill = new SolidColorBrush(Colors.LightGray);
+                rec19.Fill = new SolidColorBrush(Colors.LightGray);
+                rec28.Fill = new SolidColorBrush(Colors.LightGray);
+                rec20.Fill = new SolidColorBrush(Colors.LightGray);
+                rec27.Fill = new SolidColorBrush(Colors.LightGray);
+                rec21.Fill = new SolidColorBrush(Colors.LightGray);
+                rec22.Fill = new SolidColorBrush(Colors.LightGray);
+                rec23.Fill = new SolidColorBrush(Colors.LightGray);
+                rec24.Fill = new SolidColorBrush(Colors.LightGray);
+                rec25.Fill = new SolidColorBrush(Colors.LightGray);
+                rec26.Fill = new SolidColorBrush(Colors.LightGray);
+            }
+
+            if (op3.IsSelected)
+            {
+
+            }
+
+        }
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
     }
 }
