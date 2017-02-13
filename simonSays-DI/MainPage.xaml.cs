7ce21afd6aef7319b523b1b90c082162e78156be
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2017-02-13 12:54:28 +0100

    Ya sabemos que rectangulo elige el usuario

--- simonSays-DI/MainPage.xaml.cs
+++ simonSays-DI/MainPage.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
+﻿using Windows.UI;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -103,5 +94,12 @@ namespace simonSays_DI
             this.Frame.Navigate(typeof(MainPage));
         }
 
+        private async void rect_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MessageDialog dialog = new MessageDialog("Has pulsadoooooooooo!!!!!", "Funcionaaaaaaaaaaaaa");
+            
+            dialog.ShowAsync();
+
+        }
     }
 }
