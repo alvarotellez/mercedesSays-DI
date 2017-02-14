d7dce52492780c420604039b460d092083d839ce
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2017-02-14 12:18:49 +0100

    Bien subido

--- simonSays-DI/MainPage.xaml.cs
+++ simonSays-DI/MainPage.xaml.cs
@@ -24,7 +24,6 @@ namespace simonSays_DI
         {
             this.InitializeComponent();
 
-<<<<<<< HEAD
             if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
             {
                 row1.Height = new GridLength(0);
@@ -41,15 +40,16 @@ namespace simonSays_DI
                 col5.Width = new GridLength(0);
                 col6.Width = new GridLength(0);
             }
-            else if(AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile") {
+            else if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
+            {
 
                 row2.Height = new GridLength(0);
-                row3.Height = new GridLength(250/2);
-                row4.Height = new GridLength(250/2);
+                row3.Height = new GridLength(250 / 2);
+                row4.Height = new GridLength(250 / 2);
                 row5.Height = new GridLength(0);
 
                 col2.Width = new GridLength(0);
-                col3.Width = new GridLength(250 /2);
+                col3.Width = new GridLength(250 / 2);
                 col4.Width = new GridLength(250 / 2);
                 col5.Width = new GridLength(0);
 
@@ -58,14 +58,13 @@ namespace simonSays_DI
 
 
 
-=======
-            
+
 
             //DEFAULT
             row1.Height = new GridLength(0);
             row2.Height = new GridLength(0);
-            row3.Height = new GridLength(border.Height / 2);
-            row4.Height = new GridLength(border.Height / 2);
+            row3.Height = new GridLength(250 / 2);
+            row4.Height = new GridLength(250 / 2);
             row5.Height = new GridLength(0);
             row6.Height = new GridLength(0);
 
@@ -77,46 +76,12 @@ namespace simonSays_DI
             col6.Width = new GridLength(0);
 
             ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(1500, 1550));
->>>>>>> master
         }
-        private void comboNivel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            private void comboNivel_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {   
 
             if (op1.IsSelected)
             {
-                //rec36.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec35.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec34.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec33.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec32.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec31.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec17.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec30.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec18.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec29.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec19.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec28.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec20.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec27.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec21.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec22.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec23.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec24.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec25.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec26.Fill = new SolidColorBrush(Colors.LightGray);
-
-                //rec5.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec16.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec15.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec14.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec6.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec13.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec7.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec12.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec8.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec9.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec10.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec11.Fill = new SolidColorBrush(Colors.LightGray);
 
 
                 
@@ -144,26 +109,6 @@ namespace simonSays_DI
 
             if (op2.IsSelected)
             {
-                //rec36.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec35.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec34.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec33.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec32.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec31.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec17.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec30.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec18.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec29.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec19.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec28.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec20.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec27.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec21.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec22.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec23.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec24.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec25.Fill = new SolidColorBrush(Colors.LightGray);
-                //rec26.Fill = new SolidColorBrush(Colors.LightGray);
 
                 row1.Height = new GridLength(0);
                 row2.Height = new GridLength(border.Height / 4);
